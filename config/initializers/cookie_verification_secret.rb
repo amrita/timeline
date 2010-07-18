212c76062fdcca83a20596dceec0eeb5221185dc
thakur.amrita@gmail.com <thakur.amrita@gmail.com>
2010-07-17 20:27:36 -0700

    adding the new public files. downgrading to ruby 2.3.5 and commenting out the cookie key.

--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '554fe242dd95e637d18179f48958aad61ea94989052d8854af2394a0d60daf6cf9db3520a9ad463fed1edce2a6fb0d3d84c8f9dae664688ac0bc1df74615163f';
+# ActionController::Base.cookie_verifier_secret = '554fe242dd95e637d18179f48958aad61ea94989052d8854af2394a0d60daf6cf9db3520a9ad463fed1edce2a6fb0d3d84c8f9dae664688ac0bc1df74615163f';
